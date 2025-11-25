6b20fe6a276945f2afd9484b360d3c1aa5059027
Ignacio Igarza <186641593+IgnacioIgarza@users.noreply.github.com>
2025-11-25 20:28:17 -0300

    Primera subida de archivos

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Inicio() {
+  return (
+    <div style={{ padding: '20px' }}>
+        <br />
+        <br />
+        <h2 style={{ marginBottom: '10px' }}>Servicios</h2>
+        <hr />
+        <h3>Ofrecemos servicios veterinarios a domicilio.</h3>
+        <p>Contáctenos para brindarle atención médica de calidad a sus mascotas en la comodidad de su hogar.</p>
+        <br />
+        <p>- Consultas veterinarias, diagnóstico clínico</p>
+        <p>- Laboratorio</p>
+        <p>- Vacunación</p>
+        <p>- Desparasitaciones</p>
+        <p>- Ozonoterapia</p>
+        <br />
+        <br />
+        <br />
+
+    </div>
+  )
+}
+
+export default Inicio
